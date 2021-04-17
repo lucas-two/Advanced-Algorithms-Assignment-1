8700971b83cade85090f8eca8814b721da981bb6
Lucas Geurtjens <Lucas.Geurtjens@griffithuni.edu.au>
2021-04-17 11:46:06 +1000

    Converting to class (WIP) q4

--- src/Q4/q4.cpp
+++ src/Q4/q4.cpp
@@ -3,54 +3,84 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+#define N 10 // Maximum integer
 
-// class Map //O(1)
-// {
-//     const
+void printList(vector<int>, int[N]);
+void clear(vector<int>, int[N]);
 
-//     // private:
-//     //     int data[]; // N + 1 size
+class myStructure
+{
+private:
+    int const NN = 10;
+    int lookup[N];
 
-//     //     int hash(int key)
-//     //     {
-//     //         int index = 0;
-//     //         return index;
-//     //     }
+    vector<int> list;
+    // Initialise lookup with -1s
+    fill_n(lookup, N, -1);
 
-//     // Can store integers in the range 0 - > N (n is the upper bound)
-//     // Duplicate integers not allowed
-//     // Add, delete, test being in the list, iterating through the list
-//     // Clearing the list
-// };
-
-// struct map
-// {
-//     int value;
-//     int index;
-// };
+public:
+    /* Constructor */
+    myStructure(vector<int> inputList)
+    {
+        // Set the list to the input list
+        list = inputList;
+        // Initialise the lookup map
+        for (int i = 0; i < list.size(); i++)
+        {
+            lookup[list[i]] = i;
+        }
+    }
+    /* Clear method */
+    void clear()
+    {
+        // Set the list values in our map to -1
+        // O(K) where K is size of list
+        for (int i = 0; i < list.size(); i++)
+        {
+            lookup[list[i]] = -1;
+        }
+        // O(K) where k is size of the list
+        list.clear();
+    }
+    /* Printing out the list and lookup map*/
+    void print()
+    {
+        // List printing
+        cout << "list: {  ";
+        for (int i = 0; i < list.size(); i++)
+        {
+            cout << list[i] << "  ";
+        }
+        cout << "}" << endl;
+        // Map printing
+        cout << "map: {  ";
+        for (int i = 0; i < N; i++)
+        {
+            cout << lookup[i] << "  ";
+        }
+        cout << "}" << endl;
+    }
+};
 
 int main()
 {
-    const int N = 10;
-    // Our list of values
-    vector<int> arr{1, 4, 2};
+    vector<int> valueList{1, 4, 2};
+    myStructure obj1({1, 4, 2});
 
-    // Load the values into a hash table
-    int lookup[N];
-    // Fill the lookup with -1 values
+    // vector<int> valueList{1, 4, 2}; // valueList with values between 0->N with no duplicates
+    int lookup[N]; // Maps between valueList items and its accosciated index
+
+    // Initialise lookup with -1s
     fill_n(lookup, N, -1);
 
-    // Loading the array into our map
-    for (int i = 0; i < arr.size(); i++)
+    // Load valueList into our lookup map
+    for (int i = 0; i < valueList.size(); i++)
     {
-        lookup[arr[i]] = i;
+        lookup[valueList[i]] = i;
     }
+    printList(valueList, lookup);
+    clear(valueList, lookup);
+    printList(valueList, lookup);
 
-    for (int i = 0; i < N; i++)
-    {
-        if (lookup[i] != -1)
-        {
-            cout << i << " : " << lookup[i] << endl;
-        }
-    }
+    // clear(valueList, lookup);
 }
